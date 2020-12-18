78d99d9bf24dc3f0629a055d7ce3ec3f5fb9863f
sunsound33 <sun.sound33@gmail.com>
2020-12-18 23:54:08 +0900

    halfWay_point

---
+++ backend/database/seeds/MineTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class MineTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('mine')->truncate();
+        DB::table('mine')->insert([
+            'name' => 'whisky',
+            'age' => 29,
+        ]);
+    }
+}
